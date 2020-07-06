184fbef3b65924afc69bfd37b15ec086b4d86a14
isandeepmakwana <isandeepmakwana1@gmail.com>
2020-07-06 14:09:31 +0530

    commit 3 rd time for models file  charngee

--- main_page/models.py
+++ main_page/models.py
@@ -1,10 +1,21 @@
 from __future__ import unicode_literals
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
 
 class Item(models.Model):
     """
     This is the moodel for items.
     """
-    
+    title = models.CharField(max_length=255)
+    subtitle = models.CharField(max_length=255, blank =True, null=True)
+    like_count = models.IntegerField(default=0)
+    img = models.ImageField()
+    owner = models.ForeignKey(User)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def full_title(self):
+        return "%s :  %s" %(self.title, self.subtitle)
