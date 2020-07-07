3cc2bfca0e569401a9ef10ab84a21591f8a8be84
isandeepmakwana <isandeepmakwana1@gmail.com>
2020-07-07 22:38:39 +0530

    fifth commit

--- main_page/admin.py
+++ main_page/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Item
+
 
 # Register your models here.
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['title', 'like_count']
+    search_fields = ['title']
+    
+
+
+
+admin.site.register(Item, ItemAdmin)
