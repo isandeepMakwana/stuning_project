8ee9e344245c4858ccb0edb4b518252fdc1863df
isandeepmakwana <isandeepmakwana1@gmail.com>
2020-07-07 22:17:42 +0530

    fourth commit

--- main_page/models.py
+++ main_page/models.py
@@ -12,10 +12,11 @@ class Item(models.Model):
     subtitle = models.CharField(max_length=255, blank =True, null=True)
     like_count = models.IntegerField(default=0)
     img = models.ImageField()
-    owner = models.ForeignKey(User)
+    #owner = models.ForeignKey(User)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
     def full_title(self):
         return "%s :  %s" %(self.title, self.subtitle)
+        
