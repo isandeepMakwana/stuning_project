5e7d9819470dad6810028dc8cbf870c6d76829cc
isandeepmakwana <isandeepmakwana1@gmail.com>
2020-07-07 23:25:08 +0530

    5th commit

--- main_page/urls.py
+++ main_page/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name='index'),    
+    path('',views.index, name='index'),
+    path('items/?$',views.item.ItemList.as_viwe(), name='item-list'),
+    path('items/(?P<pk>[0-9]+)/?$', views.item.ItemList.as_viwe())    
 ]
